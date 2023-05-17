4553fb961ff7c29e381c9e3367c0ba0baf65a176
Enzogslf <119141032+Enzogslf@users.noreply.github.com>
2023-05-17 20:23:35 +0000

    primitive concept
    also i had to create new test file because i couldnt commit by cli in my slax linux install
    info about it is on the readme
    far from good, but its a starting point at least

--- test.java
+++ test.java
@@ -1,4 +1,24 @@
 public class test {
-    String test = "1x2+5a";
-    
+	public static void main(String[] args){
+		
+	String test = "1x^2+5a";
+	int plus =  test.lastIndexOf("+");
+	int minus = test.lastIndexOf("-");
+	//System.out.println(plus); < for debugging ig
+	//System.out.println(minus);
+	
+	if(plus != -1 ){
+
+	//System.out.println("hello"); < more debugging i guess
+	String afterplus = test.substring(plus+1);
+
+	String beforeplus = test.substring(0, plus);
+	System.out.println("a:"+ beforeplus);
+	System.out.println("b:"+ afterplus);
+	}
+	if(minus != -1){
+		String afterminus = test.substring(minus+1);
+		System.out.println(afterminus);
+	}
+}
 }
