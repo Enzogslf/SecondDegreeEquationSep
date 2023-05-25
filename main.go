1430a6ee876ca93300a7b379c81f7063141a4d92
Enzogslf <cyudef@gmail.com>
2023-05-25 13:55:41 -0300

    baseline

--- main.go
+++ main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+func input() string {
+	var input string
+	fmt.Scanln(&input)
+	return input
+	// fmt.Println(input)
+
+}
+func indexOf(str string) int {
+	sum := input()
+	var index int = strings.IndexAny(sum, str)
+	fmt.Println(index)
+	return index
+}
 
 func main() {
-	fmt.Println("hello from main!")
+	indexOf("x")
+}
+
+//*currently not working but im taking a short break so i want to commit the changes
+
+func allIndexOf(str string) int {
+	sum := input()
+	var characterCheck bool
+	var locationOfX int
+	LoXIncreaser := locationOfX + 1
+	characterCheck = strings.Contains(sum, str)
+	if characterCheck == true {
+		locationOfX = strings.IndexAny(sum, str)
+		for locationOfX <= 0 {
+			locationOfX = strings.IndexAny(sum, str)
+			LoXIncreaser++
+			fmt.Println(locationOfX)
+			return locationOfX
+		}
+	}
+	// var a int = strings.Index(input, str)	// return a
+	return 0
 }
